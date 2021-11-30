bcc5ab8215f97fe9d6aaed991e8b5c1703339d31
Bitos88 <tyrion72@gmail.com>
2021-11-30 15:40:01 +0100

    Initial Commit

---
+++ BitoGuessTheFlag/BitoGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitoGuessTheFlagApp.swift
+//  BitoGuessTheFlag
+//
+//  Created by Alberto Alegre Bravo on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BitoGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
