3071b2eb714cc15f13d27a271b56aa81bd84c436
Bitos88 <tyrion72@gmail.com>
2021-11-30 16:46:59 +0100

    app adivina la bandera

--- BitoGuessTheFlag/ContentView.swift
+++ BitoGuessTheFlag/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showingScore = false
+    @State var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctaAnswer = Int.random(in: 0...2)
+    
+    @State private var score = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.mint
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("GUESS THE FLAG")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                Spacer()
+                VStack (spacing: 20){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctaAnswer])
+                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                Spacer()
+                VStack{
+                    Text("Your Score Is: \(score)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your Score is \(score)")
+        }
     }
+    
+    func flagTapped(_ number: Int)  {
+        if number == correctaAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+            score -= 1
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctaAnswer = Int.random(in: 0...2)
+    }
+    
+    
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
